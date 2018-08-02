bd957343095c5b7c6243eb04d27d388dd4fd3595
Ri0S <aa248424@gmail.com>
2018-08-02 03:27:01 +0000

    fix

--- models.py
+++ models.py
@@ -70,6 +70,6 @@ class Coarse(nn.Module):
             pred_words = [torch.cat((pred_words[idx], words[idx])) for idx in range(coarse_data.size(1))]
 
             coarse_hidden = self.coarse_enc(pred_words, pred_length if len(coarse_length[1:-1]) == 0 else coarse_length[1:-1].sum(0) + pred_length)
-            total_length = (coarse_length[1:-1].sum(0) + pred_length).sum()
+            total_length = coarse_length[1:-1].sum() + pred_length.sum()
         return coarse_hidden, loss / total_length.item()
 
